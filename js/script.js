78fa0d9d6e2dd8fa999395f39b24ff432e64ea3d
Michał Hajduk <michal.hajduk@e3.pl>
2019-09-18 12:33:49 +0200

    add class active to articles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict'
+/*document.getElementById('test-button').addEventListener('click', function(){
+  const links = document.querySelectorAll('.titles a');
+  console.log('links:', links);
+});*/
+const titleClickHandler = function(event){
+  console.log('Link was clicked!');
+  console.log(event);
+  /* remove class 'active' from all article links  */
+
+}
+
+  /* add class 'active' to the clicked link */
+console.log('clickedElement:', clickedElement);
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post.active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a')
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}

322e4b02254fd9a628d5e36538ed8d2e771c3532
Michał Hajduk <michal.hajduk@e3.pl>
2019-09-26 11:56:20 +0200

    problem with html link

--- js/script.js
+++ js/script.js
@@ -26,8 +26,45 @@
 
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
+
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+  /* remove contents of titleList */
+  function clearMessages(){
+    const titleList = document.querySelector(optTitleListSelector);
+	   document.getElementById('messages').innerHTML = titleList;
+     console.log(document.getElementById('messages'));
+     console.log(titleList);
+  }
+  function generateTitleLinks(){
+    clearMessages();
+    /* for each article */
+    const articles = document.querySelectorAll('.post');
+    for(let article of articles){
+
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+
+      /* find the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* create HTML of the link */
+      const linkHtml = '<li><a href = "#"' + articleId + '><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+  }
+
+  /* get the title from the title element */
+
+  /* insert link into titleList */
+
+}
+
+generateTitleLinks();
  }
- 
+
  const links = document.querySelectorAll('.titles a');
  for(let link of links){
    link.addEventListener('click', titleClickHandler);
